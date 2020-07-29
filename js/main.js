e46c58cf8edb64f25756e69dac32716de23abfca
Sergey56106 <sss_s_1986@mail.ru>
2020-07-29 06:26:17 +0300

    cfixed:hotel slide img& responsive for reviews

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   loop: true,
   // Navigation arrows
   navigation: {
-    nextEl: ".hotel-slider__button--next",
-    prevEl: ".hotel-slider__button--prev",
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
 });
